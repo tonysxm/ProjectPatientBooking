ceb4e88fc1f63a7fa848b126500640f947daa4ef
Sherwin Pantophlet <sherwin.pantophlet@portavita.nl>
2018-03-10 18:24:10 +0100

    add demo bookings page

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../service/auth.service";
+import {AuthService} from '../service/auth.service';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {BookingSlot} from '../models/bookingslot';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-bookings',
@@ -8,13 +11,28 @@ import {AuthService} from "../service/auth.service";
 })
 export class BookingsComponent implements OnInit {
 
-  constructor(private auth: AuthService) {
+    public bookingsref: AngularFirestoreCollection<BookingSlot>;
+    public bookingsref$: Observable<{}>;
+    public bookingSlotsref: AngularFirestoreCollection<BookingSlot>;
+    public bookingSlotsref$: Observable<BookingSlot>;
+
+    constructor(private auth: AuthService, public afs: AngularFirestore) {
       if (!auth.isAuthenticated()) {
           auth.login();
       }
-  }
+      this.bookingsref = afs.collection('bookings');
+      this.bookingsref$ = this.bookingsref.snapshotChanges();
+      // this.bookingsref$ = this.bookingsref.snapshotChanges().map(actions => {
+      //     return actions.map(action => {
+      //         const data = action.payload.doc.data();
+      //         const id = action.payload.doc.id;
+      //         return { id, ...data };
+      //     });
+      // });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+    }
 
 }
